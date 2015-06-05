506d97b7f783d83174163b6c8d3eb8bb0e04de4d
Ross <Ross@Rostislavs-MacBook-Pro.local>
2015-06-05 16:04:55 +0300

    Personal website with some JavaScript

---
+++ MyWebsite/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("MyWebsite")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("MyWebsite")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b43caeb4-f6ca-40cf-b4d3-3f02940f5e92")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ MyWebsite/javascript/myScript.js
@@ -0,0 +1,60 @@
+﻿
+    //create the coming soon text
+    var bookCover = document.getElementById('cover');
+    var bookText = document.getElementById('coverText');
+    bookCover.addEventListener('mouseover', function (ev) {
+        bookText.innerHTML = 'Coming Soon';
+    });
+    bookCover.addEventListener('mouseout', function (ev) {
+        bookText.innerHTML = '';
+    });
+
+  
+
+    var article = document.getElementById('articlesLi'),
+        ul = document.getElementById('secondUl');
+
+    article.addEventListener('mouseover', function (e) {
+
+        ul.style.visibility = 'visible';
+
+    });
+
+    article.addEventListener('mouseout', function (e) {
+        ul.style.visibility = 'hidden';
+    });
+
+    function validateForm() {
+        
+        var mail = document.forms["subscribe"]["mail"].value;
+        if (mail == null || mail == "") {
+            alert("Please Enter Your Email Address");
+            return false;
+        }
+    }
+
+   
+
+
+
+
+       
+        
+
+     
+
+              
+        
+        
+
+
+        
+
+      
+     
+        
+
+ 
+       
+    
+
